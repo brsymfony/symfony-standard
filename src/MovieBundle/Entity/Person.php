ad57b873bce579ce73e1b0ffb95bf2d5ab18678f
fmorante <francisco.morante@berepublic.es>
2015-07-06 18:21:21 +0200

    entities + getters & setters

--- src/MovieBundle/Entity/Person.php
+++ src/MovieBundle/Entity/Person.php
@@ -8,4 +8,69 @@ class Person
     protected $movies;
     protected $moviesDirected;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMovies()
+    {
+        return $this->movies;
+    }
+
+    /**
+     * @param mixed $movies
+     */
+    public function setMovies($movies)
+    {
+        $this->movies = $movies;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMoviesDirected()
+    {
+        return $this->moviesDirected;
+    }
+
+    /**
+     * @param mixed $moviesDirected
+     */
+    public function setMoviesDirected($moviesDirected)
+    {
+        $this->moviesDirected = $moviesDirected;
+    }
+
+
 }
--- src/MovieBundle/Entity/Producer.php
+++ src/MovieBundle/Entity/Producer.php
@@ -7,4 +7,53 @@ class Producer
     protected $name;
     protected $movies;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMovies()
+    {
+        return $this->movies;
+    }
+
+    /**
+     * @param mixed $movies
+     */
+    public function setMovies($movies)
+    {
+        $this->movies = $movies;
+    }
+
+    
 }
